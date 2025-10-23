a72cbc0dc6f19e6b159a82d0a8cefc1a7bf1464f
Kenzo8845 <silvabazanikenai@gmail.com>
2025-10-23 02:06:30 -0300

    Adicionada a implementação em chao.c

---
+++ src/chao.c
@@ -0,0 +1,87 @@
+#include "chao.h"
+#include "fila.h"
+#include "formas.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct chao {
+    Fila formas; 
+} EstruturaChao;
+
+Chao chao_cria(Fila geo_formas) {
+    EstruturaChao *c = (EstruturaChao*) malloc(sizeof(EstruturaChao));
+    if (c == NULL) {
+        printf("Erro(0) em chao_cria");
+        return NULL;
+    }
+
+    c->formas = geo_formas; 
+
+    return (Chao)c;
+}
+
+void chao_destruir(Chao c) {
+    EstruturaChao *chao = (EstruturaChao*) c;
+    if (chao == NULL) {
+        printf("Erro(0) em chao_destruir");
+        return;
+    }
+
+    while (!fila_vazia(chao->formas)) {
+        void *forma_generica = fila_retiraDoInicio(chao->formas);
+        if (forma_generica != NULL) {
+            forma_destruir((Forma)forma_generica); 
+        }
+    }
+
+    fila_destruir(chao->formas);
+
+    free(chao);
+}
+
+Forma chao_retiraForma(Chao c){
+    EstruturaChao *chao = (EstruturaChao*) c;
+    if (chao == NULL || fila_vazia(chao->formas)) {
+        printf("Erro(0) em chao_retiraForma");
+        return NULL;
+    }
+
+    void* formaRetirada = fila_retiraDoInicio(chao->formas);
+    if(formaRetirada == NULL) {
+        printf("Erro(1) em chao_retiraForma");
+        return NULL;
+    }
+
+    return (Forma)formaRetirada;
+}
+
+void chao_adicionaAoChao(Chao c, Forma f) {
+    EstruturaChao *chao = (EstruturaChao*) c;
+    if (chao == NULL || chao->formas == NULL || f == NULL) {
+        printf("Erro(0) em chao_tamanho");
+        return;
+    }
+
+    fila_adicionaAoFim(chao->formas, (void*)f);
+}
+
+int chao_getTamanho(Chao c) {
+    EstruturaChao *chao = (EstruturaChao*) c;
+    if (chao == NULL || chao->formas == NULL) {
+        printf("Erro(0) em chao_tamanho");
+        return -1;
+    }
+    
+    return fila_tamanho(chao->formas);
+}
+
+Fila chao_getFila(Chao c) {
+    EstruturaChao *chao = (EstruturaChao*) c;
+    if (chao == NULL || chao->formas == NULL) {
+        printf("Erro(0) em chao_getFila");
+        return NULL;
+    }
+
+    return chao->formas;
+}
---
+++ src/chao.h
@@ -0,0 +1,88 @@
+#ifndef CHAO_H
+#define CHAO_H
+
+#include "fila.h"
+#include "formas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* TAD de chão.
+* O chão é um tipo de fila, com logica de primerio a entrar sera o primeiro a sair.
+* Todos os elementos armazenados no chão serão formas.
+* O chão serve de como "inventario" das formas utilizadas no jogo da bocha.
+*
+* Quando o chão é usado:
+* Ao iniciar o jogo, todas as formas lidas no arquivo .geo são adicionadas em ordem de leitura ao chão.
+* Quando uma forma é colocada de um carregador ela é pega do chão.
+* Ao sair da arena, a forma ou é destruida, ou retorna ao chão.
+*
+* Esse arquivo contem todas as funções responsaveis pela manipulação do chão.
+*/
+
+typedef struct EstruturaChao* Chao;
+
+
+
+/*=====================*/
+/* Constructor do chão */
+/*=====================*/
+/**
+ * @brief Cria um chão novo.
+ * @param geo_formas Uma fila com os as formas retiradas do arquivo geo.
+ * @return O chao criado, ou NULL em caso de erro.
+ */
+Chao chao_cria(Fila geo_formas);
+
+
+
+/*====================*/
+/* Destructor do chão */
+/*====================*/
+/**
+ * @brief Destroi e libera a memoria de um chão.
+ * @param c O chão qual sera destruido.
+ */
+void chao_destruir(Chao c);
+
+
+
+
+/*====================*/
+/* Operações do chão  */
+/*====================*/
+/**
+ * @brief Retira-se uma forma do inicio do chão.
+ * @param c O chão de qual a forma sera retirda.
+ * @return Forma, A forma retirada, ou NULL em caso de erro.
+ */
+Forma chao_retiraForma(Chao c);
+
+/**
+ * @brief Adiciona uma forma ao final do chão.
+ * @param c O chão no qual sera adicionada a forma.
+ * @param f A forma na qual sera adicionada ao chão.
+ */
+void chao_adicionaAoChao(Chao c, Forma f);
+
+
+
+/*====================*/
+/*  Getters do chão   */
+/*====================*/
+/**
+ * @brief Pega a quantidade de Formas no chao.
+ * @param c O chão qual se quer saber o tamanho.
+ * @return int O numero de formas no chão, ou -1, em caso de erro.
+ */
+int chao_getTamanho(Chao c);
+
+/**
+ * @brief Retorna a Fila do chão selecionado.
+ * @param c O chão qual se quer a fila.
+ * @return Fila A fila do chão selecionado, ou NULL, em caso de erro.
+ */
+Fila chao_getFila(Chao c);
+
+
+#endif
